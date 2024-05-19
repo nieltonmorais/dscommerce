60a1cb1715b037c66cc7e590f22c3a7a7e8a41bb
Nielton Morais <nielton.morais@gmail.com>
2024-05-19 15:58:10 -0300

    first commit dscommerce

---
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dscommerce;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Running...");
+	}
+}
